bdacda2d5c5c873a16d4ee819661058f4ac6264b
vclalcanfor <vinicius.alcanfor@gmail.com>
2020-08-10 18:18:53 -0300

    adicionando o blink

---
+++ blink.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python 
+#Autor:Vitor Mazuco 
+#Contato:contato@vmzsolutions.com.br 
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+
+while (True):
+	GPIO.output(18, True)
+	time.sleep(0.5)
+	GPIO.output(18, False)
+	time.sleep(0.5)
