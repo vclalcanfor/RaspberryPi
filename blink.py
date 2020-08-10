039965307337ff931629ec3b8e177c74cb0f60ed
vclalcanfor <vinicius.alcanfor@gmail.com>
2020-08-10 18:21:23 -0300

    mudando o pino do 18 para o 16

--- blink.py
+++ blink.py
@@ -6,10 +6,10 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
 
 while (True):
-	GPIO.output(18, True)
+	GPIO.output(16, True)
 	time.sleep(0.5)
-	GPIO.output(18, False)
+	GPIO.output(16, False)
 	time.sleep(0.5)
